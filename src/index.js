ea3fd4c4c5d51a47b589a315b701f91282dc7194
omar ite <omar.ite@gmail.com>
2021-01-18 08:12:07 -0800

    Final with unit testing s Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import Films from './Components/Films/Films';
+import Characters from "./Components/Characters/Characters"
+
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/characters/:filmId" component={Characters} />
+      <Route path="/">
+        <Films />
+      </Route>
+    </Switch>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById("root"));
